9743ca8f16da346af6a8e528a3d83191e9028a62
raviyadav4568 <30981862+raviyadav4568@users.noreply.github.com>
2017-08-18 00:47:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.get('/', function (req, res) {
 
 app.get('/htmlone.html',function(req, res){
     
-    res.sendFile(path.join(_dirname, 'ui', 'htmlone.html'));
+    res.sendFile(path.join(__dirname, 'ui', 'htmlone.html'));
     
 });
 
